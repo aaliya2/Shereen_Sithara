1dafe0084142901f40bc5439799b35ad0cad23b9
aaliya2 <sithara24@gmail.com>
2020-10-10 12:02:29 +0530

    Changes made

--- Oct_10_2020_CI_Devops_Jenkin/src/Devops_Jenkin.java
+++ Oct_10_2020_CI_Devops_Jenkin/src/Devops_Jenkin.java
@@ -4,6 +4,7 @@ public class Devops_Jenkin {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Shereen Sithara");
+		System.out.println("Changes Made");
 	}
 
 }
